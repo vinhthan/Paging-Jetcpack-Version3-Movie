1870b29919299a8e77b18497d73b7d1b03bf3e69
vinhthan <thanmanhvinh@gmail.com>
2021-09-24 17:31:38 +0700

    cm

--- app/src/main/java/com/example/pagingjetpackversion3demo/MoviePagingSource.kt
+++ app/src/main/java/com/example/pagingjetpackversion3demo/MoviePagingSource.kt
@@ -1,5 +1,6 @@
 package com.example.pagingjetpackversion3demo
 
+import android.net.Uri
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.pagingjetpackversion3demo.network.RetrofitService
@@ -19,6 +20,18 @@ class MoviePagingSource(private val apiService: RetrofitService): PagingSource<I
             val response = apiService.getMoviePopular("034bbd1b233d6726e0c7dc7f338657f9", nextPage)
 
             LoadResult.Page(data = response.results, prevKey = null, nextKey = response.page + 1)
+
+            //
+            /*val nextPage = params.key ?: FIRST_PAGE
+            val response = apiService.getDataFromAPI(nextPage)
+            var nextPageNumber: Int? = null
+            if (response.info.next != null) {
+                val uri = Uri.parse(response.info.next)
+                val nextPageQuery = uri.getQueryParameter("page")
+                nextPageNumber = nextPageQuery?.toInt()
+            }
+
+            LoadResult.Page(data = response.results, prevKey = null, nextKey = nextPageNumber)*/
 
         } catch (e: Exception) {
             LoadResult.Error(e)
